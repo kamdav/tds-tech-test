6f87008c1eae598423a147336414dcf0725424fb
Kam Dav <kam-dav@hotmail.com>
2025-08-24 15:07:34 +0100

    Add select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { Select } from './components/select'
 
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <Select
+        title="Currency from"
+        selectName="currencyFrom"
+      />
     </div>
   )
 }
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,3 +1,17 @@
-export const Select: React.FC = () => {
-  return <select></select>
+type SelectProps = {
+  title: string;
+  selectName: string;
+}
+
+export const Select: React.FC<SelectProps> = ({
+  title,
+  selectName
+}: SelectProps) => {
+  return <>
+    <label htmlFor={selectName}>{title}:</label>
+    <select name={selectName}>
+      <option value="testOption1">Option 1</option>
+      <option value="testOption2">Option 2</option>
+    </select>
+  </>
 }
